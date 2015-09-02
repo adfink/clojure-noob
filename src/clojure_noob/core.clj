b8d081eee2da900a85607aedf58f959a4fa1ea0f
Andrew Fink <andrewfink8@gmail.com>
2015-09-02 14:50:05 -0700

    first clojure project

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,12 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "start your engines"))
+(println "cleanliness is next to godliness")
+(defn train
+  []
+  (println "Choo choo!"))
+(map)
