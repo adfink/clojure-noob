18c941ba8ef472ab02cb3a3e3dbaf2b8c3461ffb
Andrew Fink <andrewfink8@gmail.com>
2015-09-03 16:00:20 -0700

    functions n such

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -9,4 +9,19 @@
 (defn train
   []
   (println "Choo choo!"))
-(map)
+(+ 1 (* 2 3) 4)
+
+
+(if true
+  "abra cadabra"
+  "hocus pocus")
+(defn i-like-food
+   [food]
+   (str "MMMMM, I love me some " food "!!!")
+     )
+
+(defn more-food
+  [& foods]
+  (map i-like-food foods))
+
+(println (more-food "ice cream " "pizzza " "peanut butter jelly time"))
