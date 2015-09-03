8c7cc5059c26e4423be944e7c0f4fb7e57111ed5
Andrew Fink <andrewfink8@gmail.com>
2015-09-03 16:57:12 -0700

    hobbit exercise

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -25,3 +25,44 @@
   (map i-like-food foods))
 
 (println (more-food "ice cream " "pizzza " "peanut butter jelly time"))
+
+
+;Hobbit exercise
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
